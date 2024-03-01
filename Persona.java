6c9fd9933e6c27bf9690169b41582194e4d0e93a
270503-chan <8355@itescam.edu.mx>
2024-03-01 16:29:09 -0600

    se creo un archivo

---
+++ Persona.java
@@ -0,0 +1,11 @@
+public class Persona{
+  String nombre;
+  int matricula;
+
+  public void main (String[]args);
+  System.out.println("Dianelly de los Angeles Ku Chan");
+  System.out.println("8355");
+
+  {
+
+{
