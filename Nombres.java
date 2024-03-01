a9ef36c1c791af2740c13bdcbeb0f3f4457e8874
AlejandraDzul <8343@itescam.edu.mx>
2024-03-01 15:47:57 -0600

    archivo con mi nombre

--- Nombres.java
+++ Nombres.java
@@ -1,7 +1,8 @@
 public class Nombres{
   public static void main(String[] args){
      System.out.println("Dianelly de los Angeles Ku Chan");
+     System.out.println("Leticia Alejandra Dzul Lopez");
 
-
+  }
 
 }
