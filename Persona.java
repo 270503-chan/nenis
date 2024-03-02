df0c765167813da29054f0ee4300164f5b2c64a4
AlejandraDzul <8343@itescam.edu.mx>
2024-03-01 18:54:48 -0600

    correcion de codigo

--- Persona.java
+++ Persona.java
@@ -2,7 +2,7 @@ public class Persona{
   String nombre;
   int matricula;
 
-  public void main (String[]args){
+  public static void main (String[]args){
   System.out.println("Leticia Alejandra Dzul Lopez");
   System.out.println("8343");
   System.out.println("Dianelly de los Angeles Ku Chan");
