30d7c23c0d6198cf180c13e26c5c07cfe038fa6a
AlejandraDzul <8343@itescam.edu.mx>
2024-03-01 18:38:01 -0600

    actualizacion de codigo

---
+++ Persona.java
@@ -0,0 +1,10 @@
+public class Persona{
+  String nombre;
+  int matricula;
+
+  public void main (String[]args){
+  System.out.println("Leticia Alejandra Dzul Lopez");
+  System.out.println("8343");
+
+  }
+}
